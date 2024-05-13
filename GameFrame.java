d598363d3acdb53faf03334af01cfbc76b42ff6d
Berker <karakusberker@gmail.com>
2024-05-13 17:19:16 +0300

    Implemented Paddle and made modifications in GameFrame and panel

--- GameFrame.java
+++ GameFrame.java
@@ -1,5 +1,19 @@
-public class GameFrame {
+import java.awt.*;
+import javax.swing.*;
+public class GameFrame extends JFrame {
+    GamePanel panel=new GamePanel();
 
+     public GameFrame(){
+
+        this.add(panel);
+        this.setTitle("Retro Hockey Game");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
 
 
 }
--- GamePanel.java
+++ GamePanel.java
@@ -1,2 +1,13 @@
-public class GamePanel {
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
 }
