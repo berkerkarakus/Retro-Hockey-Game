735c92f2db2a364f3ef13faca2138b3621aa370e
efekankavakli <efekan.kavakli@ozu.edu.tr>
2024-05-13 17:36:48 +0300

    game panel and score updated

--- Score.java
+++ Score.java
@@ -1,2 +1,14 @@
-public class Score {
+import java.awt.*;
+
+public class Score extends Rectangle {
+
+    static int GAME_WIDTH;
+    static int GAME_HEIGHT;
+    int player1;
+    int player2;
+
+    Score(int GAME_WIDTH, int GAME_HEIGHT){
+        Score.GAME_WIDTH = GAME_WIDTH;
+        Score.GAME_HEIGHT = GAME_HEIGHT;
+    }
 }
