5180cb1a9f0dc94902eed46718f96aac73b811ae
Vesilya Gezer <vesilya.gezer@ozu.edu.tr>
2024-05-14 19:20:41 +0300

    Missing methods of GamePanel class, additional features

--- GamePanel.java
+++ GamePanel.java
@@ -60,8 +60,75 @@ public class GamePanel extends JPanel implements Runnable {
         paddle2.move();
         ball.move();
     }
-    @Override
-    public void run() {
+    public void checkCollision() {
 
+        //bounce ball off top & bottom window edges
+        if(ball.y <=0) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+        if(ball.y >= GAME_HEIGHT-BALL_DIAMETER) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+        //bounce ball off paddles
+        if(ball.intersects(paddle1)) {
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity++; //optional for more difficulty
+            if(ball.yVelocity>0)
+                ball.yVelocity++; //optional for more difficulty
+            else
+                ball.yVelocity--;
+            ball.setXDirection(ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+        }
+        if(ball.intersects(paddle2)) {
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity++; //optional for more difficulty
+            if(ball.yVelocity>0)
+                ball.yVelocity++; //optional for more difficulty
+            else
+                ball.yVelocity--;
+            ball.setXDirection(-ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+        }
+        //stops paddles at window edges
+        if(paddle1.y<=0)
+            paddle1.y=0;
+        if(paddle1.y >= (GAME_HEIGHT-PADDLE_HEIGHT))
+            paddle1.y = GAME_HEIGHT-PADDLE_HEIGHT;
+        if(paddle2.y<=0)
+            paddle2.y=0;
+        if(paddle2.y >= (GAME_HEIGHT-PADDLE_HEIGHT))
+            paddle2.y = GAME_HEIGHT-PADDLE_HEIGHT;
+        //give a player 1 point and creates new paddles & ball
+        if(ball.x <=0) {
+            score.player2++;
+            newPaddles();
+            newBall();
+            System.out.println("Player 2: "+score.player2);
+        }
+        if(ball.x >= GAME_WIDTH-BALL_DIAMETER) {
+            score.player1++;
+            newPaddles();
+            newBall();
+            System.out.println("Player 1: "+score.player1);
+        }
+    }
+    public void run() {
+        //game loop
+        long lastTime = System.nanoTime();
+        double amountOfTicks =60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        while(true) {
+            long now = System.nanoTime();
+            delta += (now -lastTime)/ns;
+            lastTime = now;
+            if(delta >=1) {
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+            }
+        }
     }
 }
