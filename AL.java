bf5a1e4677ff71768899f2272d3b8d8e76afe751
ecemozbiyik <ecem.ozbiyik@ozu.edu.tr>
2024-05-14 19:34:25 +0300

    added AL class.

---
+++ AL.java
@@ -0,0 +1,15 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class AL extends KeyAdapter {
+    public void keyPressed(KeyEvent e) {
+        paddle1.keyPressed(e);
+        paddle2.keyPressed(e);
+    }
+    public void keyReleased(KeyEvent e) {
+        paddle1.keyReleased(e);
+        paddle2.keyReleased(e);
+    }
+}
+
+
