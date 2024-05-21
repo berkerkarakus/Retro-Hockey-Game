ab8a9984aa32b44f06bd237c9570ed671b1ceaac
Berker <karakusberker@gmail.com>
2024-05-21 20:33:04 +0300

    Modifications on Some Classes are made and issue solve

--- Paddle.java
+++ Paddle.java
@@ -7,52 +7,42 @@ public class Paddle extends Rectangle{
     int id;
     int yVelocity;
     int speed = 10;
+    boolean[] keyStates = new boolean[256];  // Adjust the size if needed for more keys
 
-    Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, String id1,int id){
-        super(x,y,PADDLE_WIDTH,PADDLE_HEIGHT);
-        this.id1=id1;
-        this.id=id;
+    Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, String id1, int id) {
+        super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
+        this.id1 = id1;
+        this.id = id;
     }
 
     public void keyPressed(KeyEvent e) {
-        switch(id) {
-            case 1:
-                if(e.getKeyCode()==KeyEvent.VK_W) {
-                    setYDirection(-speed);
-                }
-                if(e.getKeyCode()==KeyEvent.VK_S) {
-                    setYDirection(speed);
-                }
-                break;
-            case 2:
-                if(e.getKeyCode()==KeyEvent.VK_UP) {
-                    setYDirection(-speed);
-                }
-                if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-                    setYDirection(speed);
-                }
-                break;
-        }
+        keyStates[e.getKeyCode()] = true;  // Set the key state to pressed
     }
+
     public void keyReleased(KeyEvent e) {
-        switch(id) {
-            case 1:
-                if(e.getKeyCode()==KeyEvent.VK_W) {
-                    setYDirection(0);
-                }
-                if(e.getKeyCode()==KeyEvent.VK_S) {
-                    setYDirection(0);
-                }
-                break;
-            case 2:
-                if(e.getKeyCode()==KeyEvent.VK_UP) {
-                    setYDirection(0);
-                }
-                if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-                    setYDirection(0);
-                }
-                break;
+        keyStates[e.getKeyCode()] = false;  // Set the key state to not pressed
+    }
+
+    public void move() {
+        if (id == 1) {
+            if (keyStates[KeyEvent.VK_W]) {
+                setYDirection(-speed);
+            } else if (keyStates[KeyEvent.VK_S]) {
+                setYDirection(speed);
+            } else {
+                setYDirection(0);
+            }
+        } else if (id == 2) {
+            if (keyStates[KeyEvent.VK_UP]) {
+                setYDirection(-speed);
+            } else if (keyStates[KeyEvent.VK_DOWN]) {
+                setYDirection(speed);
+            } else {
+                setYDirection(0);
+            }
         }
+
+        y += yVelocity;
     }
     public void setYDirection(int yDirection) {
         yVelocity = yDirection;
@@ -62,9 +52,7 @@ public class Paddle extends Rectangle{
         return yVelocity;
     }
 
-    public void move() {
-        y= y + yVelocity;
-    }
+
     public void draw(Graphics g) {
         if(id1=="Fenerbahce") {
 
